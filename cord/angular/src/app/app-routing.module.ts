66d40e3080c9ea580e06f92a87c52ce188d49b9e
MD. SANAULLAH <158471899+mdsanaullah2030@users.noreply.github.com>
2024-07-09 10:05:41 +0600

    8.07.2024

--- cord/angular/src/app/app-routing.module.ts
+++ cord/angular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NusraatComponent } from './nusraat/nusraat.component';
+import { LocationComponent } from './location/location.component';
 
 const routes: Routes = [
-  {path:'nusrat',component:NusraatComponent}
+  {path:'nusrat',component:NusraatComponent},
+  {path:'location',component:LocationComponent},
 ];
 
 @NgModule({
--- cord/angular/src/app/app.module.ts
+++ cord/angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NusraatComponent } from './nusraat/nusraat.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NusraatComponent
+    NusraatComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
